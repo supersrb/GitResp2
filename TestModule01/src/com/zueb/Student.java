269bb04883ba60dfd9ae39859701093646fdb584
1579939516@qq.com <1579939516@qq.com>
2024-03-07 19:38:32 +0800

    实行一次克隆后的push操作

--- TestModule01/src/com/zueb/Student.java
+++ TestModule01/src/com/zueb/Student.java
@@ -7,9 +7,9 @@ package com.zueb;
  */
 public class Student {
     public static void main(String[] args) {
-
         System.out.println("第一次更改");
         System.out.println("你好");
         System.out.println("实行一次提交和推送同时进行");
+        System.out.println("实行一次克隆后的push操作");
     }
 }
