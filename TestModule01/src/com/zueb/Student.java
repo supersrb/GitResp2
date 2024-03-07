f637223ab4523e9fbfb1c31c44fee9f7c4cac3f4
1579939516@qq.com <1579939516@qq.com>
2024-03-07 14:10:18 +0800

    第一次进行数据的提交

--- TestModule01/src/com/zueb/Student.java
+++ TestModule01/src/com/zueb/Student.java
@@ -2,11 +2,14 @@ package com.zueb;
 
 /**
  * @author: supersrb(1579939516 @ qq.com)
- * @Date: 2024年03月07日 13:51
+ * @Date: 2024年03月07日 14:07
  * @Description:
  */
 public class Student {
+    private int age;
+    private String name;
     public static void main(String[] args) {
-        System.out.println("第一次提交到本地库");
+
+        System.out.println("你好");
     }
 }
