f82f8d727c9db1b21fc8e7aee8c2d623e113906b
1579939516@qq.com <1579939516@qq.com>
2024-03-07 20:46:47 +0800

    制造一次冲突

--- TestModule01/src/com/zueb/Student.java
+++ TestModule01/src/com/zueb/Student.java
@@ -13,5 +13,6 @@ public class Student {
         System.out.println("实行一次克隆后的push操作");
 		System.out.println("制造一次冲突 BY 工作区");
         System.out.println("制造一次冲突 BY 工作区");
+        System.out.println("制造一次冲突 BY IDEA");
     }
 }
